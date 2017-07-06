d7acdd65f91349b404832c9de351ad2ef21f2258
Yotam Salmon <yotam.salmon@gmail.com>
2017-07-06 19:41:00 +0300

    Facebook API

--- facebook-chat-api/extension.js
+++ facebook-chat-api/extension.js
@@ -1,9 +1,13 @@
 (function(ext) {
-    var socket;
-    window.waiting = {};
-    var waiting = window.waiting;
+    var waiting = {};
     var last_error = "";
-    var logged_in = false;
+    var logged_in = false; 
+
+    var details = {};
+
+    window.msgQueue = [];
+    window.readyNextMsg = true;
+    window.currentMsg = null;
 
     // Cleanup function when the extension is unloaded
     ext._shutdown = function() {
@@ -19,32 +23,26 @@
     };
 
     ext.connect = function(callback) {
+        if (window.socket && socket.readyState == WebSocket.OPEN) {
+            callback();
+            return; 
+        }
         socket = new WebSocket("ws://localhost:9888");
         socket.onopen = function() {
             callback();
         };
         socket.onmessage = function(d) {
+            console.log("received " + d.data);
             var data = JSON.parse(d.data);
+            if (data.event) {
+                console.log("MESSAGE!");
+                msgQueue.push(data.body);
+            }
             if (waiting[data.id]) {
-                var callback = waiting[data.id].callback;
-                var custom = waiting[data.id].custom;
-                var error = waiting[data.id].error;
                 if (!data.success) {
                     last_error = data.error;
-                    if (error) {
-                        error();
-                    }
-                    callback();
                 }
-                else if (custom) {
-                    callback(custom(data));
-                }
-                else if (data.value) {
-                    callback(data.value)
-                }
-                else {
-                    callback();
-                }
+                waiting[data.id](data);
                 delete waiting[data.id];
             }
         };
@@ -52,24 +50,168 @@
 
     ext.login = function(email, password, callback) {
         var session = Math.random().toString();
-        waiting[session] = {callback: callback, custom: (x) => {
-            logged_in = true;
-        }, error: (x) => {
-            logged_in = false;
-        }};
+        waiting[session] = function(d) {
+            logged_in = d.success;
+            callback();
+        }
         socket.send(JSON.stringify({action: "login", id: session, email: email, password: password}));
     };
 
     ext.logged_in = function() {
         return logged_in;
-    }
+    };
+ 
+    ext.send_message = function(msg, recipient, callback) {
+        var session = Math.random().toString();
+        waiting[session] = function(d) {
+            callback(d.messageID);
+        };
+        socket.send(JSON.stringify({action: "sendMessage", recipient: recipient, message: msg, id: session}));
+    };
+
+    ext.get_id_user = function(nm, callback) {
+        var session = Math.random().toString();
+        waiting[session] = function(d) {
+            callback(d.account);
+        };
+        socket.send(JSON.stringify({action: "getID", name: nm, id: session}));
+    };
+
+    ext.get_details_user = function(id, callback) {
+        var session = Math.random().toString();
+        waiting[session] = function(d) {
+            details[id] = d.details;
+            callback();
+        };
+        socket.send(JSON.stringify({action: "getDetails", userID: id, id: session}));
+    };
+
+    ext.get_details_name = function(id) {
+        return details[id] ? details[id].name : "";
+    };
+    ext.get_details_first_name = function(id) {
+        return details[id] ? details[id].firstName : "";
+    };
+    ext.get_details_vanity = function(id) {
+        return details[id] ? details[id].vanity : "";
+    };
+    ext.get_details_vanity = function(id) {
+        return details[id] ? details[id].vanity : "";
+    };
+    ext.get_details_thumbnail = function(id) {
+        return details[id] ? details[id].thumbSrc : "";
+    };
+    ext.get_details_profile = function(id) {
+        return details[id] ? details[id].profileUrl : "";
+    };
+    ext.get_details_gender = function(id) {
+        return details[id] ? details[id].gender : "";
+    };
+    ext.get_details_friend = function(id) {
+        return details[id] ? details[id].isFriend : "";
+    };
+    ext.get_details_bd = function(id) {
+        return details[id] ? details[id].isBirthday : "";
+    };
+
+    ext.set_title = function(group, title, callback) {
+        var session = Math.random().toString();
+        waiting[session] = function(d) {
+            callback();
+        };
+        socket.send(JSON.stringify({action: "setTitle", threadID: group, title: title, id: session}));
+    };
+
+    ext.add_to_group = function(user, group, callback) {
+        var session = Math.random().toString();
+        waiting[session] = function(d) {
+            callback();
+        };
+        socket.send(JSON.stringify({action: "addUserToGroup", threadID: group, userID: user, id: session}));
+    };
+
+    ext.remove_from_group = function(user, group, callback) {
+        var session = Math.random().toString();
+        waiting[session] = function(d) {
+            callback();
+        };
+        socket.send(JSON.stringify({action: "removeUserFromGroup", threadID: group, userID: user, id: session}));
+    };
+
+    ext.change_thread_color = function(thread, color, callback) {
+        var session = Math.random().toString();
+        waiting[session] = function(d) {
+            callback();
+        };
+        socket.send(JSON.stringify({action: "changeThreadColor", threadID: thread, color: color, id: session}));
+    };
+
+    ext.create_poll = function(question, group, options, callback) {
+        var session = Math.random().toString();
+        waiting[session] = function(d) {
+            callback();
+        };
+        var opts = {};
+        var o = options.split(',');
+        for (var i = 0; i < o.length; i++) {
+            opts[o[i]] = false;
+        }
+        console.log(opts);
+        socket.send(JSON.stringify({action: "createPoll", threadID: group, question: question, options: opts, id: session}));
+    };
+
+    ext.check_next_msg = function() {
+        if (msgQueue.length > 0 && readyNextMsg) {
+            readyNextMsg = false;
+            currentMsg = msgQueue.shift();
+            return true;
+        }
+        return false;
+    };
+
+    ext.next_message = function() {
+        readyNextMsg = true;
+    };
 
     // Block and block menu descriptions
     var descriptor = {
         blocks: [
             ['w', 'Open connection to the server', 'connect'],
             ['w', 'Login with %s, %s', 'login', 'email@provider.com', 'password'],
-            ['b', 'Is logged in?', 'logged_in']
+            ['b', 'Is logged in?', 'logged_in'],
+
+            ['-'],['-'],
+
+            ['w', 'Send message %s to %s', 'send_message', 'text', 'recipient id'],
+            ['R', 'Send message %s to %s and get message ID', 'send_message', 'text', 'recipient id'],
+
+            ['-'],['-'],
+
+            ['R', 'ID of %s', 'get_id_user', 'name'],
+            ['w', 'Request details of %n', 'get_details_user', 'id'],
+            ['r', 'Name of %n', 'get_details_name', 'id'],
+            ['r', 'First name of %n', 'get_details_first_name', 'id'],
+            ['r', 'Vanity of %n', 'get_details_vanity', 'id'],
+            ['r', 'Thumbnail URL of %n', 'get_details_thumbnail', 'id'],
+            ['r', 'Profile URL of %n', 'get_details_profile', 'id'],
+            ['b', 'Is it birthday of %n', 'get_details_bd', 'id'],
+            ['b', 'Is %n a friend?', 'get_details_friend', 'id'],
+            
+            ['-'],['-'],
+
+            ['w', 'Set %n\'s title to %s', 'set_title', 'group', 'title'],
+            ['w', 'Add user %n to group %n', 'add_to_group', 'id', 'group'],
+            ['w', 'Remove user %n from group %n', 'remove_from_group', 'id', 'group'],
+            ['w', 'Change group %n color to #%s', 'change_thread_color', '', 'FFFFFF'],
+
+            ['-'],['-'],
+
+            ['w', 'Create poll %s in %n with options %s', 'create_poll', 'question', 'group', 'options(a,b,c..)'],
+            
+            ['-'],['-'],
+
+            ['h', 'When there\'s a message', 'check_next_msg'],
+            [' ', 'Finish message handling', 'next_message']
         ]
     };
 
--- facebook-chat-api/index.js
+++ facebook-chat-api/index.js
@@ -6,24 +6,30 @@ var clients = {}
 const server = new ws({port: 9888});
 server.on("connection", function(conn) {
     conn.on("message", function(data) {
-        try {
+        //try {
             var msg = JSON.parse(data);
             actions[msg.action](conn, msg);
-        }
+        /*}
         catch (e) {
             try {
-                conn.send(JSON.stringify({id: msg.id, success: false, err: "Internal server error"}));
+                conn.send(JSON.stringify({id: msg.id, success: false, err: "Internal server error: " + JSON.stringify(e)}));
             }
             catch (e2) { }
-        }
+        }*/
     });
     conn.on("close", function() {
-        if (clients[conn]) delete clients[conn];
+        if (clients[conn]) {
+			if (clients[conn].stopListening) {
+				clients[conn].stopListening();
+			}
+			clients[conn].api.logout(function(){});
+			delete clients[conn];
+		}
     })
 });
 
 function loggedin(conn, msg) {
-    var api = clients[conn];
+    var api = clients[conn] ? clients[conn]["api"] : null;
     if (!api) {
         conn.send(JSON.stringify({id: msg.id, success: false, err: "No connected account"}));
         return false;
@@ -42,7 +48,13 @@ var actions =
                 conn.send(JSON.stringify({id: msg.id, success: false, error: err.error}));
                 return;
             }
-            clients[conn] = api;
+            clients[conn] = {api: api, stopListening: 
+				api.listen(function(err, event) {
+					if (event.type == "message") {
+						conn.send(JSON.stringify({event: "message", body: event, sender: event.threadID}));
+					}
+				})
+			};
             conn.send(JSON.stringify({id: msg.id, success: true}));
         });
     },
@@ -58,7 +70,7 @@ var actions =
                 conn.send(JSON.stringify({id: msg.id, success: false, error: err.error}));
                 return;
             }
-            conn.send(JSON.stringify({id: msg.id, success: true}));
+            conn.send(JSON.stringify({id: msg.id, success: true, messageID: messageInfo.messageID}));
         });
     },
     /*
@@ -69,11 +81,146 @@ var actions =
         if (!api) return;
 
         api.getUserID(msg.name, function(err, data) {
-            if (err) {
+            if (err) {a
                 conn.send(JSON.stringify({id: msg.id, success: false, error: err.error}));
                 return;
             }
             conn.send(JSON.stringify({id: msg.id, success: true, account: data[0].userID}));
+        });
+    },
+    /*
+    Fields: userID
+    */
+	"getDetails": function(conn, msg) {
+		var api = loggedin(conn, msg);
+        if (!api) return;
+		
+		api.getUserInfo([msg.userID], function(err, ret) {
+            if (err) {
+                conn.send(JSON.stringify({id: msg.id, success: false, error: err.error}));
+                return;
+            }
+            conn.send(JSON.stringify({id: msg.id, success: true, details: ret[msg.userID]}));
+        });
+	},
+    /*
+    Fields: threadID, title
+    */
+    "setTitle": function(conn, msg) {
+        var api = loggedin(conn, msg);
+        if (!api) return;
+
+        api.setTitle(msg.title, msg.threadID, function(err, ret) {
+            if (err) {
+                conn.send(JSON.stringify({id: msg.id, success: false, error: err.error}));
+                return;
+            }
+            conn.send(JSON.stringify({id: msg.id, success: true}));
+        });
+    },
+    /*
+    Fields: userID, threadID
+    */
+    "addUserToGroup": function(conn, msg) {
+        var api = loggedin(conn, msg);
+        if (!api) return;
+
+        api.addUserToGroup(msg.userID, msg.threadID, function(err, ret) {
+            if (err) {
+                conn.send(JSON.stringify({id: msg.id, success: false, error: err.error}));
+                return;
+            }
+            conn.send(JSON.stringify({id: msg.id, success: true}));
+        });
+    },
+    /*
+    Fields: userID, threadID
+    */
+    "removeUserFromGroup": function(conn, msg) {
+        var api = loggedin(conn, msg);
+        if (!api) return;
+
+        api.removeUserFromGroup(msg.userID, msg.threadID, function(err, ret) {
+            if (err) {
+                conn.send(JSON.stringify({id: msg.id, success: false, error: err.error}));
+                return;
+            }
+            conn.send(JSON.stringify({id: msg.id, success: true}));
+        });
+    },
+    /*
+    Fields: threadID, color
+    */
+    "changeThreadColor": function(conn, msg) {
+        var api = loggedin(conn, msg);
+        if (!api) return;
+
+        api.changeThreadColor(msg.color, msg.threadID, function(err, ret) {
+            if (err) {
+                conn.send(JSON.stringify({id: msg.id, success: false, error: err.error}));
+                return;
+            }
+            conn.send(JSON.stringify({id: msg.id, success: true}));
+        });
+    },
+    /*
+    Fields: threadID, emoji
+    */
+    "changeThreadEmoji": function(conn, msg) {
+        var api = loggedin(conn, msg);
+        if (!api) return;
+
+        api.changeThreadColor("🇨🇭", msg.threadID, function(err, ret) {
+            if (err) {
+                conn.send(JSON.stringify({id: msg.id, success: false, error: err.error}));
+                return;
+            }
+            conn.send(JSON.stringify({id: msg.id, success: true}));
+        });
+    },
+    /*
+    Fields: question, options {str: bool}, threadID
+    */
+    "createPoll": function(conn, msg) {
+        var api = loggedin(conn, msg);
+        if (!api) return;
+
+        api.createPoll(msg.question, msg.threadID, msg.options, function(err, ret) {
+            if (err) {
+                conn.send(JSON.stringify({id: msg.id, success: false, error: err.error}));
+                return;
+            }
+            conn.send(JSON.stringify({id: msg.id, success: true}));
+        });
+    },
+    /*
+    Fields: messageID
+    */
+    "deleteMessage": function(conn, msg) {
+        var api = loggedin(conn, msg);
+        if (!api) return;
+
+        api.deleteMessage(msg.messageID, function(err, ret) {
+            if (err) {
+                conn.send(JSON.stringify({id: msg.id, success: false, error: err.error}));
+                return;
+            }
+            conn.send(JSON.stringify({id: msg.id, success: true}));
+        });
+    },
+    /*
+    Fields: threadID
+    */
+    "deleteThread": function(conn, msg) {
+        var api = loggedin(conn, msg);
+        if (!api) return;
+
+        api.deleteThread(msg.threadID, function(err, ret) {
+            if (err) {
+                conn.send(JSON.stringify({id: msg.id, success: false, error: err.error}));
+                return;
+            }
+            conn.send(JSON.stringify({id: msg.id, success: true}));
         });
     }
 }
